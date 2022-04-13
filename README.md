19faeea5a78bd4f8f154c6144650c3aa9476ba8f
EarlLessen <102614411+EarlLessen@users.noreply.github.com>
2022-04-13 09:32:07 -0700

    Updated Readme

---
+++ README.md
@@ -0,0 +1,42 @@
+# FormikDemo
+
+A basic React Native Hello World application using Formik.
+
+## Description
+
+This demo shows how to use Formik to create a simple form. The base code is React Native for Web, it can be run for the browser or the simulator.
+
+## Getting Started
+
+Clone this project into your project folder or download from GitHub.
+
+### Installing
+
+On your terminal: 
+
+cd to your project folder -> FormikDemo
+
+npm install
+
+cd ios && pod install
+
+### Executing program
+
+**To run on the browser:**
+
+terminal: npm run web
+
+open http://localhost:3000/
+
+**To run on the iOS simulator:**
+
+terminal: npm run ios`
+
+## Resources for Formik
+Docs - https://formik.org/docs/overview
+
+React Nstive - https://formik.org/docs/guides/react-native
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
--- src/screens/Demo/Demo.tsx
+++ src/screens/Demo/Demo.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import {
-  Button,
-  SafeAreaView,
-  TextInput,
-  View,
-  StyleSheet,
-  Text,
-} from 'react-native';
+import {styles} from './styles';
+import {Button, SafeAreaView, TextInput, View, Text} from 'react-native';
 import {Formik} from 'formik';
 import {demoValidationSchema} from './DemoValidation';
 
 export const Demo = () => (
   <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={styles.header}>Formik</Text>
+    <View style={styles.header}>
+      <Text style={styles.headerText}>Formik</Text>
     </View>
     <Formik
       validationSchema={demoValidationSchema}
-      initialValues={{email: ''}}
+      initialValues={{name: '', email: ''}}
       onSubmit={values => console.log(values)}>
       {({handleChange, handleBlur, handleSubmit, values}) => (
         <View style={styles.form}>
+          <Text style={styles.textLabel}>username</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder={'enter your name'}
+            onChangeText={handleChange('name')}
+            onBlur={handleBlur('name')}
+            value={values.name}
+          />
+          <Text style={styles.textLabel}>email</Text>
           <TextInput
             style={styles.textInput}
             placeholder={'enter your email'}
@@ -34,26 +37,4 @@ export const Demo = () => (
     </Formik>
   </SafeAreaView>
 );
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  form: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  header: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
-  },
-  textInput: {
-    width: '100%',
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-  },
-});
 export default Demo;
